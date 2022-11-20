2609d017dbba01a247f0b1601c9ae400bb70a251
VineethMarthala <117797052+VineethMarthala@users.noreply.github.com>
2022-11-20 13:39:58 +0530

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateTest
+{
+   public  class Employee
+    {
+        public virtual int Id { get; set; }
+        public virtual string FirstName { get; set; }
+
+        public virtual string LastName { get; set; }
+    }
+}
