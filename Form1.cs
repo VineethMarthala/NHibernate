125167d03f9458e3bc8135795a2fefee1ade5a7f
VineethMarthala <117797052+VineethMarthala@users.noreply.github.com>
2022-11-20 13:41:28 +0530

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NHibernateTest
+{
+    public partial class Form1 : Form
+    {
+        private Configuration myConfiguration;
+        private ISessionFactory mySessionFactory;
+        private ISession mySession;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myConfiguration = new Configuration();
+            myConfiguration.Configure();
+            mySessionFactory = myConfiguration.BuildSessionFactory();
+            mySession = mySessionFactory.OpenSession();
+
+            //Add
+            using (mySession.BeginTransaction())
+            {
+                Employee emp = new Employee {Id=1, FirstName = "VIneeth", LastName="Reddy" };
+                mySession.Save(emp);
+                mySession.Transaction.Commit();
+            }
+
+            //Fetching Data From DB
+            using (mySession.BeginTransaction())
+            {
+                ICriteria critera = mySession.CreateCriteria<Employee>();
+                IList<Employee> list = critera.List<Employee>();
+
+                
+
+                mySession.Transaction.Commit();
+            }
+
+            using (mySession.BeginTransaction())
+            {
+                var emp = mySession.QueryOver<Employee>().Where(x => x.Id == 1);
+                mySession.Delete(emp);
+                mySession.Transaction.Commit();
+            }
+        }
+    }
+}
